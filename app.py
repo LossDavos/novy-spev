d140ece56f42e9a3d9a118351cca5bea17357b2e
DDDavidSt <david.stevanak@gmail.com>
2025-10-16 10:21:04 +0200

    Added delete functionlaity

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ S3_BUCKET = os.getenv("S3_BUCKET")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
-DELETE_SONG_PASSWORD = os.getenv("DELETE_SONG_PASSWORD", "DELETE_SONG_2024")  # Default fallback
+DELETE_SONG_PASSWORD = os.getenv("DELETE_SONG_PASSWORD")  # Default fallback
 
 
 
